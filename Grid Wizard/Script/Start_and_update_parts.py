c3e2f0cc319c7af7f104b6e2571220bb37c9e824
GRYPHINKING <134159377+GRYPHINKING@users.noreply.github.com>
2023-10-12 15:22:14 -0600

    OFFICIAL initial SMOKE Project

---
+++ Grid Wizard/Script/Start_and_update_parts.py
@@ -0,0 +1,25 @@
+﻿def Start_App_Update_Parts():
+    #Clicks the 'DesktopWindowXamlSource' object.
+    Aliases.explorer.wndShell_TrayWnd.DesktopWindowXamlSource.Click(1067, 24)
+    #Clicks the '|Desktop|ProgramData' item of the 'tvNamespaceTreeControl' tree.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink.NamespaceTreeControl.tvNamespaceTreeControl.ClickItem("|Desktop|ProgramData")
+    #Double-clicks the 'Name' object.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink2.ShellView.Items_View.AutoSPRINK_2023.Name.DblClick(36, 5)
+    #Clicks the 'Name' object.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink2.ShellView.Items_View.Parts_mdb.Name.Click(26, 8)
+    #Enters '[Del]' in the 'Parts_mdb' object.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink2.ShellView.Items_View.Parts_mdb.Keys("[Del]")
+    #Clicks the 'Name' object.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink2.ShellView.Items_View.Parts_mdb.Name.Click(26, 8)
+    #Enters '[Del]' in the 'Parts_mdb' object.
+    Aliases.explorer.wndHome.Home.DUIViewWndClassName.Explorer_Pane.CtrlNotifySink2.ShellView.Items_View.Parts_mdb.Keys("[Del]")
+    #Closes the 'wndHome' window.
+    Aliases.explorer.wndHome.Close()
+    #Runs the "AutoSPRINKx64" tested application.
+    TestedApps.AutoSPRINKx64.Run(1, True)
+    #Delays the test execution for the specified time period.
+    Delay(8000, "Wait for Update Parts Dialog")
+    #Clicks the 'btnYes' button.
+    Aliases.AutoSPRINKx64.dlgGridSystemIntroduction.btnYes.ClickButton()
+    #Clicks the 'btnOK' button.
+    Aliases.AutoSPRINKx64.dlgGridSystemIntroduction.btnOK.ClickButton()
---
+++ Grid Wizard/Script/System_Optimizer.py
@@ -0,0 +1,17 @@
+﻿def System_Optimizer():
+    #Clicks the 55097 item of the 'Main' toolbar.
+    Aliases.AutoSPRINKx64.wndAfx.Afx.Main.ClickItem(55097, False)
+    #Clicks the 37537 item of the 'Wizards' toolbar.
+    Aliases.AutoSPRINKx64.wndAfx.Afx2.Wizards.ClickItem(37537, False)
+    #Clicks the 'btnFinish' button.
+    Aliases.AutoSPRINKx64.dlgGridSystemIntroduction.btnFinish.ClickButton()
+    #Compares the Grid_Check Stores item with the image of the Regions.CreateRegionInfo(Aliases.AutoSPRINKx64.wndAfx, 29, 79, 1781, 274, False) object.
+    Regions.Grid_Check.Check(Regions.CreateRegionInfo(Aliases.AutoSPRINKx64.wndAfx, 29, 79, 1781, 274, False))
+    #Clicks the 37140 item of the 'Wizards' toolbar.
+    Aliases.AutoSPRINKx64.wndAfx.Afx2.Wizards.ClickItem(37140, False)
+    #Clicks the 'btn' button.
+    Aliases.AutoSPRINKx64.dlgGridSystemIntroduction.btn.ClickButton()
+    #Compares the SystemOptimizer_dialog Stores item with the image of the Aliases.AutoSPRINKx64.dlgGridSystemIntroduction object.
+    Regions.SystemOptimizer_dialog.Check(Aliases.AutoSPRINKx64.dlgGridSystemIntroduction, False, False, 23967)
+    #Closes the 'dlgGridSystemIntroduction' window.
+    Aliases.AutoSPRINKx64.dlgGridSystemIntroduction.Close()
